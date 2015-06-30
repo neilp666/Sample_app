2da71239987a4f52292de90a56e8a8edef0dfae5
neil patel <neil@myindianweddingapp.com>
2015-06-30 16:51:43 +0100

    Added header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,12 @@
 module ApplicationHelper
 
   #returns the full title on a per-page basis.
-  def full_title(page_title = '')
-    base_title = "Ruby on Rails Tutorial Sample App"
-      if page_title.empty?
+  def full_title(page_title = '') # Method def, optional arg
+    base_title = "Ruby on Rails Tutorial Sample App" # Variable assignment
+      if page_title.empty? #Boolean Test
         base_title
       else
-        page_title + " | " + base_title
+        page_title + " | " + base_title # String concatenation
       end
   end
 end
