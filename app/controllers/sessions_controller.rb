1d3b2b362bc802e81228957a2eb9dfe1727ff52a
neil patel <neil@myindianweddingapp.com>
2015-07-07 16:11:52 +0100

    updated destroy and create method in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,22 @@
 class SessionsController < ApplicationController
   def new
   end
+
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      #log the user in and redirect to the users show page
+      log_in user
+      redirect_to user
+    else
+      flash.new[:dander] = "Invalid email/password combination"
+      render 'new'
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the sample app!"
       redirect_to @user  #redurect_to user_urk(@user)
     else
